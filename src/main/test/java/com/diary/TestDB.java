93fa68162ceae724bf00819ba392745b81ebc524
17 <1150804209@qq.com>
2021-11-26 20:54:07 +0800

    test

--- src/main/test/java/com/diary/TestDB.java
+++ src/main/test/java/com/diary/TestDB.java
@@ -2,10 +2,23 @@ package com.diary;
 
 
 import com.diary.util.DBUtil;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestDB {
+
+    private static Logger logger = LoggerFactory.getLogger(DBUtil.class);
+
+
+    @Test
     public void testConnection(){
         System.out.println(DBUtil.getConnection());
+
+        logger.info("在{}时，获取数据库连接", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
 
 }
